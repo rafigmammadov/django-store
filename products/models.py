7bf549ec0eeb4a6c0f4b9dd721bb177fe69b119e
rafigmammadov <rafigmammadov@yahoo.com>
2023-05-24 15:33:50 +0400

    Models added to models.py Migrations were made, models were migrated

--- products/models.py
+++ products/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class ProductCategory(models.Model):
+    name = models.CharField(max_length=128, null=False, unique=True)
+    description = models.TextField(null=True, blank=True)
+
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=256, null=False)
+    description = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=256, decimal_places=2)
+    image = models.ImageField(upload_to='products_images')
+    quantity = models.PositiveIntegerField()
+    category = models.ForeignKey(to=ProductCategory, on_delete=models.CASCADE)
