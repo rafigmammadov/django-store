66289fbf11722bbd73b3b6194004aba5dda0e9ff
rafigmammadov <rafigmammadov@yahoo.com>
2023-07-11 23:57:49 +0400

    Login form was created, login template was corrected

--- users/forms.py
+++ users/forms.py
@@ -1,2 +1,25 @@
 from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
+from users.models import User
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4', 'placeholder': 'Username'}))
+    password = forms.CharField(
+        widget=forms.PasswordInput(attrs={'class': 'form-control py-4', 'placeholder': 'Password'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
+
+
+class UserRegistrationForm(UserCreationForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2')
+
+# class UserProfileForm(UserChangeForm):
+#     class Meta:
+#         model = User
+#         fields =
--- users/views.py
+++ users/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from users.forms import UserLoginForm, UserRegistrationForm
+from django.urls import reverse
+from django.contrib import auth
 
 
 def login(request):
-    return render(request, 'users/login.html')
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = auth.authenticate(username=username, password=password)
+            if user:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+
+    context = {'form': form}
+
+    return render(request, 'users/login.html', context)
 
 
 def register(request):
