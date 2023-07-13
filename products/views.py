57bf2e60d872656c2897bf5f3f356390091e1168
rafigmammadov <rafigmammadov@yahoo.com>
2023-07-13 16:22:18 +0400

    Controllers to add and remove products to/from basket were added

--- products/views.py
+++ products/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from .models import ProductCategory, Product
+from django.shortcuts import render, HttpResponseRedirect
+from .models import ProductCategory, Product, Basket
 
 
 def index(request):
@@ -17,3 +17,24 @@ def products(request):
         'categories': ProductCategory.objects.all(),
     }
     return render(request, 'products/products.html', context)
+
+
+def basket_add(request, product_id):
+    product = Product.objects.get(id=product_id)
+    baskets = Basket.objects.filter(user=request.user, product=product)
+
+    if not baskets.exists():
+        Basket.objects.create(user=request.user, product=product, quantity=1)
+    else:
+        basket = baskets.first()
+        basket.quantity += 1
+        basket.save()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
+
+
+def basket_remove(request, basket_id):
+    basket = Basket.objects.get(id=basket_id)
+    basket.delete()
+
+    return HttpResponseRedirect(request.META['HTTP_REFERER'])
