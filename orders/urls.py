dd419a9ce677a5400b641a920b5ed14e0c965f71
rafigmammadov <rafigmammadov@yahoo.com>
2023-08-04 16:11:01 +0400

    The basic version of payment has been connected (Stripe)

--- orders/urls.py
+++ orders/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path
-from orders.views import CreateOrderTemplateView, OrderTemplateView, OrdersTemplateView, SuccessTemplateView
+from orders.views import CreateOrderTemplateView, OrderTemplateView, OrdersTemplateView, SuccessTemplateView, \
+    CanceledTemplateView
 
 app_name = 'orders'
 
-
 urlpatterns = [
-    path ('order_create', CreateOrderTemplateView.as_view(), name='order_create' ),
-    path ('orders', OrdersTemplateView.as_view(), name='orders' ),
-    path ('order', OrderTemplateView.as_view(), name='order' ),
-    path ('success',SuccessTemplateView.as_view(), name='success' ),
+    path('order_create', CreateOrderTemplateView.as_view(), name='order_create'),
+    path('orders', OrdersTemplateView.as_view(), name='orders'),
+    path('order', OrderTemplateView.as_view(), name='order'),
+    path('success', SuccessTemplateView.as_view(), name='success'),
+    path('cancel', CanceledTemplateView.as_view(), name='cancel')
 ]
--- orders/views.py
+++ orders/views.py
@@ -1,7 +1,13 @@
+import stripe
+
+from django.conf import settings
+from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.views.generic.base import TemplateView
 from django.views.generic.edit import CreateView
-from django.urls import reverse_lazy
+from django.urls import reverse, reverse_lazy
+
+from http import HTTPStatus
 
 from orders.forms import OrderCreateForm
 from orders.models import Order
@@ -12,6 +18,23 @@ class CreateOrderTemplateView(CreateView):
     template_name = 'orders/order-create.html'
     form_class = OrderCreateForm
     success_url = reverse_lazy('orders:success')
+
+    stripe.api_key = settings.STRIPE_SECRET_KEY
+
+    def post(self, request, *args, **kwargs):
+        super(CreateOrderTemplateView, self).post(request, *args, **kwargs)
+        checkout_session = stripe.checkout.Session.create(
+            line_items=[
+                {
+                    'price': 'price_1NbM0NHq11uemsKDDxnJIggJ',
+                    'quantity': 1,
+                },
+            ],
+            mode='payment',
+            success_url=f"{settings.DOMAIN_NAME}{reverse('orders:success')}",
+            cancel_url=f"{settings.DOMAIN_NAME}{reverse('orders:cancel')}")
+
+        return HttpResponseRedirect(checkout_session.url, HTTPStatus.SEE_OTHER)
 
     def get_context_data(self, **kwargs):
         context = super(CreateOrderTemplateView, self).get_context_data()
@@ -45,3 +68,7 @@ class SuccessTemplateView(TemplateView):
     def get_context_data(self, **kwargs):
         context = super(SuccessTemplateView, self).get_context_data()
         context['title'] = 'Store - The successful order'
+
+
+class CanceledTemplateView(TemplateView):
+    template_name = 'orders/cancel.html'
