310a4ca48afd3586ad878d2954e7df2407b92d0c
rafigmammadov <rafigmammadov@yahoo.com>
2023-08-10 23:11:35 +0400

    The configuration for vercel has been added

--- store/settings.py
+++ store/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-=w37=df23a!rm#$c4_@km5d4@yoi*ti)k_xi)6dm_0-y0*9j0m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
 
 DOMAIN_NAME = 'http://127.0.0.1:8000'
 
@@ -160,9 +161,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
