2452a5e1fdea308ae65009a9625e8fff345e1e61
rafigmammadov <rafigmammadov@yahoo.com>
2023-08-10 20:48:33 +0400

    The optional database has been added

--- store/settings.py
+++ store/settings.py
@@ -105,6 +105,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 # Caches
 
 CACHES = {
